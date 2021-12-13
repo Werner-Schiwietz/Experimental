876a7d4a29554febe68af4391986b640ed90ade5
WernerS <ws.github@gisbw.de>
2021-12-13 22:39:28 +0100

    texte und umgestalten

--- UndoRedo/UndoRedo/UndoRedo.cpp
+++ UndoRedo/UndoRedo/UndoRedo.cpp
@@ -20,53 +20,106 @@ namespace UT_UndoRedo
 			{
 				--i;
 			};
-			auto redo = [&i]()->void 
+			auto doing = [&i]()->void 
 			{
 				++i;
 			};
 
-			auto _do = [&]()
+			Assert::IsTrue(i==0);
+			undo_redo.AddAndDo(doing,undo);
+			Assert::IsTrue(i==1);
+			undo_redo.AddAndDo(doing,undo);
+			Assert::IsTrue(i==2);
+			undo_redo.AddAndDo(doing,undo);
+			Assert::IsTrue(i==3);
+
+			Assert::IsFalse(undo_redo.Redo());
+
+			Assert::IsTrue(undo_redo.Undo());
+			Assert::IsTrue(i==2);
+			Assert::IsTrue(undo_redo.Redo());
+			Assert::IsTrue(i==3);
+
+			Assert::IsTrue(undo_redo.Undo());
+			Assert::IsTrue(i==2);
+			Assert::IsTrue(undo_redo.Undo());
+			Assert::IsTrue(i==1);
+			Assert::IsTrue(undo_redo.Redo());
+			Assert::IsTrue(i==2);
+			Assert::IsTrue(undo_redo.Redo());
+			Assert::IsTrue(i==3);
+
+			Assert::IsTrue(undo_redo.Undo());
+			Assert::IsTrue(i==2);
+			Assert::IsTrue(undo_redo.Undo());
+			Assert::IsTrue(i==1);
+			undo_redo.AddAndDo(doing,undo);
+			Assert::IsTrue(i==2);
+			Assert::IsFalse(undo_redo.Redo());
+			Assert::IsTrue(undo_redo.Undo());
+			Assert::IsTrue(i==1);
+			Assert::IsTrue(undo_redo.Undo());
+			Assert::IsTrue(i==0);
+			Assert::IsFalse(undo_redo.Undo());
+		}
+		TEST_METHOD(TestMethod_Texte)
+		{
+			int i=0;
+			UndoRedo::VW undo_redo;
+
+			auto undo = [&i]()->void 
 			{
-				redo();//action ausführen
-				undo_redo.add(undo,redo);//
+				--i;
+			};
+			auto doing = [&i]()->void 
+			{
+				++i;
 			};
 
-			Assert::IsTrue(i==0);
-			_do();
-			Assert::IsTrue(i==1);
-			_do();
-			Assert::IsTrue(i==2);
-			_do();
-			Assert::IsTrue(i==3);
+			undo_redo.AddAndDo(doing,undo,std::make_shared<UndoRedo::DoingTextSimple>(L"0"));
+			undo_redo.AddAndDo(doing,undo,std::make_shared<UndoRedo::DoingTextSimple>(L"1"));
+			undo_redo.AddAndDo(doing,undo,std::make_shared<UndoRedo::DoingTextSimple>(L"2"));
 
-			Assert::IsFalse(undo_redo.redo());
+			auto texte = undo_redo.UndoTexte();
+			Assert::IsTrue(texte.size()==3);
+			texte = undo_redo.RedoTexte();
+			Assert::IsTrue(texte.size()==0);
 
-			Assert::IsTrue(undo_redo.undo());
-			Assert::IsTrue(i==2);
-			Assert::IsTrue(undo_redo.redo());
-			Assert::IsTrue(i==3);
+			undo_redo.Undo();
 
-			Assert::IsTrue(undo_redo.undo());
-			Assert::IsTrue(i==2);
-			Assert::IsTrue(undo_redo.undo());
-			Assert::IsTrue(i==1);
-			Assert::IsTrue(undo_redo.redo());
-			Assert::IsTrue(i==2);
-			Assert::IsTrue(undo_redo.redo());
-			Assert::IsTrue(i==3);
+			texte = undo_redo.UndoTexte();
+			Assert::IsTrue(texte.size()==2);
+			texte = undo_redo.RedoTexte();
+			Assert::IsTrue(texte.size()==1);
 
-			Assert::IsTrue(undo_redo.undo());
-			Assert::IsTrue(i==2);
-			Assert::IsTrue(undo_redo.undo());
-			Assert::IsTrue(i==1);
-			_do();
-			Assert::IsTrue(i==2);
-			Assert::IsFalse(undo_redo.redo());
-			Assert::IsTrue(undo_redo.undo());
-			Assert::IsTrue(i==1);
-			Assert::IsTrue(undo_redo.undo());
-			Assert::IsTrue(i==0);
-			Assert::IsFalse(undo_redo.undo());
+			undo_redo.Redo();
+
+			texte = undo_redo.UndoTexte();
+			Assert::IsTrue(texte.size()==3);
+			texte = undo_redo.RedoTexte();
+			Assert::IsTrue(texte.size()==0);
+
+			undo_redo.Undo();
+
+			texte = undo_redo.UndoTexte();
+			Assert::IsTrue(texte.size()==2);
+			texte = undo_redo.RedoTexte();
+			Assert::IsTrue(texte.size()==1);
+
+			undo_redo.Undo();
+
+			texte = undo_redo.UndoTexte();
+			Assert::IsTrue(texte.size()==1);
+			texte = undo_redo.RedoTexte();
+			Assert::IsTrue(texte.size()==2);
+
+			undo_redo.Undo();
+
+			texte = undo_redo.UndoTexte();
+			Assert::IsTrue(texte.size()==0);
+			texte = undo_redo.RedoTexte();
+			Assert::IsTrue(texte.size()==3);
 		}
 	};
 }
+
--- UndoRedo/UndoRedo/UndoRedoAction.h
+++ UndoRedo/UndoRedo/UndoRedoAction.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <stdexcept>
 #include <memory>
+#include <deque>
 
 namespace UndoRedo
 {
@@ -18,19 +19,22 @@ namespace UndoRedo
 		using string_t = std::wstring;
 		virtual ~IDoingText(){}
 
-		virtual string_t operator()(Direction) = 0;
+		virtual string_t const & operator()(Direction) const = 0;
 	};
 	class DoingTextNone : public IDoingText
 	{
 	public:
-		virtual string_t operator()(Direction direction) override 
+		virtual string_t const & operator()(Direction direction) const override 
 		{
+			static string_t const textUndo{L"Undo"};
+			static string_t const textRedo{L"Redo"};
+
 			switch(direction)
 			{
 			case Direction::Undo:
-				return L"Undo";
+				return textUndo;
 			case Direction::Redo:
-				return L"Redo";
+				return textRedo;
 			}
 			throw std::invalid_argument( __FUNCSIG__ " direction unknown " );
 		}
@@ -40,7 +44,7 @@ namespace UndoRedo
 		string_t text;
 	public:
 		DoingTextSimple(string_t text) : text(std::move(text)){}
-		virtual string_t operator()(Direction) override { return text; }
+		virtual string_t const & operator()(Direction) const override { return this->text; }
 	};
 	class DoingText : public IDoingText
 	{
@@ -48,7 +52,7 @@ namespace UndoRedo
 		string_t textRedo;
 	public:
 		DoingText(string_t textUndo, string_t textRedo) : textUndo(std::move(textUndo)),textRedo(std::move(textRedo)){}
-		virtual string_t operator()(Direction direction) override 
+		virtual string_t const & operator()(Direction direction) const override 
 		{ 
 			switch(direction)
 			{
@@ -61,60 +65,73 @@ namespace UndoRedo
 		}
 	};
 
-	class UndoRedoAction
-	{
-	public:
-		using action_t = std::function<void( void )>;
-		using string_t = IDoingText::string_t;
-	private:
-		Direction					direction {Direction::Undo};
-		action_t					undo_action;
-		action_t					redo_action;
-		std::shared_ptr<IDoingText>	doingtextPtr;
-
-	public:
-		UndoRedoAction( action_t undo_action, action_t redo_action, std::shared_ptr<IDoingText> doingtextPtr ) : undo_action( std::move( undo_action ) ), redo_action( std::move( redo_action ) ), doingtextPtr( std::move( doingtextPtr ) ) {}
-		UndoRedoAction( action_t undo_action, action_t redo_action ) : UndoRedoAction( std::move(undo_action), std::move(redo_action), std::make_shared<DoingTextNone>() ){}
-
-		void operator()();
-		string_t const & doing_text();
-		UndoRedoAction toggle() && ;
-	};
 
 	class VW
 	{
+		class Action
+		{
+		public:
+			using action_t = std::function<void( void )>;
+			using string_t = IDoingText::string_t;
+		private:
+			Direction					direction {Direction::Undo};
+			action_t					undo_action;
+			action_t					redo_action;
+			std::shared_ptr<IDoingText>	doingtextPtr;
+
+		public:
+			Action( action_t undo_action, action_t redo_action, std::shared_ptr<IDoingText> doingtextPtr ) : undo_action( std::move( undo_action ) ), redo_action( std::move( redo_action ) ), doingtextPtr( std::move( doingtextPtr ) ) {}
+			Action( action_t undo_action, action_t redo_action ) : Action( std::move(undo_action), std::move(redo_action), std::make_shared<DoingTextNone>() ){}
+
+		public:
+			Action				InvokeAndToggle() &&;
+			string_t const &	DoingText(Direction) const;
+		};
+		class Stack : public std::stack<Action>
+		{
+		public:
+			using base_t = std::stack<Action>;
+			base_t::container_type const & GetContainer() const { return this->c; }
+		};
 	public:
-		using action_t = UndoRedoAction::action_t;
+		using action_t = Action::action_t;
+		using textcontainer_t = std::deque<Action::string_t>;
 	protected:
-		std::stack<UndoRedoAction> undos;
-		std::stack<UndoRedoAction> redos;
+		Stack undos;
+		Stack redos;
 
 	public:
-		void add( action_t undo, action_t redo );
-		void add( action_t undo, action_t redo, std::shared_ptr<IDoingText>);
+		void AddAndDo( action_t action, action_t undo );
+		void AddAndDo( action_t action, action_t undo, std::shared_ptr<IDoingText>);
 
-		bool undo();
-		bool redo();
+		bool Undo();
+		bool Redo();
 
+		textcontainer_t UndoTexte() const {return _Texte(undos, Direction::Undo);}
+		textcontainer_t RedoTexte() const {return _Texte(redos, Direction::Redo);}
 	protected:
-		static bool		_action( std::stack<UndoRedoAction> & from, std::stack<UndoRedoAction> & to );
+		static textcontainer_t _Texte( Stack const &, Direction );
+		static bool		_action( Stack & from, Stack & to );
 		virtual void	_handle_redos();
 	};
 
-	inline void UndoRedoAction::operator()()
+	inline VW::Action					VW::Action::InvokeAndToggle() &&	
 	{
 		if( this->direction == Direction::Redo )
+		{
 			redo_action();
-		else
-			undo_action();
-	}
-	inline UndoRedoAction UndoRedoAction::toggle() && 
-	{
-		if( this->direction == Direction::Redo )
 			this->direction = Direction::Undo;
+		}
 		else
+		{
+			undo_action();
 			this->direction = Direction::Redo;
-		return std::move(*this);
+		}
+		return std::move( *this );
+	}
+	inline VW::Action::string_t const &	VW::Action::DoingText(Direction direction) const
+	{
+		return (*doingtextPtr)(direction);
 	}
 
 	inline void VW::_handle_redos()
@@ -122,34 +139,47 @@ namespace UndoRedo
 		//simple, clear redos
 		decltype(this->redos){}.swap(this->redos);
 	}
-	inline void VW::add( action_t undo, action_t redo, std::shared_ptr<IDoingText> doingtext )
-	{
-		void handle_redos();
-		undos.emplace( undo, redo, doingtext );
-	}
-	inline void VW::add( action_t undo, action_t redo )
-	{
-		_handle_redos();
-		undos.emplace( undo, redo );
-	}
-	inline bool VW::_action( std::stack<UndoRedoAction> & from, std::stack<UndoRedoAction> & to )
+	inline bool VW::_action( Stack & from, Stack & to )
 	{
 		if(from.size())
 		{
 			auto action = std::move(from.top());
 			from.pop();
-			action();
-			to.emplace(std::move(action).toggle());
+			to.emplace(std::move(action).InvokeAndToggle());
 			return true;
 		}
 		return false;
 	}
-	inline bool VW::undo()
+
+	inline bool VW::Undo()
 	{
 		return _action( this->undos, this->redos );
 	}
-	inline bool VW::redo()
+	inline bool VW::Redo()
 	{
 		return _action( this->redos, this->undos );
+	}
+
+	inline void VW::AddAndDo( action_t action, action_t undo, std::shared_ptr<IDoingText> doingtext )
+	{
+		void handle_redos();
+		action();
+		undos.emplace( undo, action, doingtext );
+	}
+	inline void VW::AddAndDo( action_t action, action_t undo)
+	{
+		_handle_redos();
+		action();
+		undos.emplace( undo, action );
+	}
+
+	inline VW::textcontainer_t VW::_Texte( Stack const & data, Direction direction )
+	{
+		VW::textcontainer_t ret_value;
+		for( auto iter = data.GetContainer().rbegin(); iter != data.GetContainer().rend(); ++iter )
+		{
+			ret_value.push_back( iter->DoingText(direction) );
+		}
+		return ret_value;
 	}
 }
